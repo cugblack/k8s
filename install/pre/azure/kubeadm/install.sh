c204bab1f14e585b37995345091a45155ec32add
cugblack <18202794429@163.com>
2018-12-14 17:51:19 +0800

    Update install.sh

--- install/pre/azure/kubeadm/install.sh
+++ install/pre/azure/kubeadm/install.sh
@@ -24,14 +24,14 @@ systemctl start docker
 #安装kubeadm kubelet kubectl: master node都需要
 
 
-      apt-get update && apt-get install -y apt-transport-https curl
-      curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-      cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-      deb https://apt.kubernetes.io/ kubernetes-xenial main
-      EOF 
-      apt-get update
-      apt-get install -y kubelet kubeadm kubectl
-      apt-mark hold kubelet kubeadm kubectl
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF 
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
 
 
 #初始化master节点
