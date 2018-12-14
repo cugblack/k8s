e3df97d72619bb6075a5b2842eb2e129bcbdc952
cugblack <18202794429@163.com>
2018-12-14 17:32:15 +0800

    Update install.sh

--- install/pre/azure/kubeadm/install.sh
+++ install/pre/azure/kubeadm/install.sh
@@ -1,5 +1,9 @@
+---
+
 # 安装docker:master node都需要
-```
+---
+
+
 apt-get remove docker-ce docker-engine docker.io
 
 apt-get update
@@ -19,22 +23,23 @@ apt-cache madison docker-ce
 apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu 
 systemctl enable docker
 systemctl start docker
-```
+
+
 # 安装kubeadm kubelet kubectl: master node都需要
 
-```
-apt-get update && apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
-```
+
+      apt-get update && apt-get install -y apt-transport-https curl
+      curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+      cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+      deb https://apt.kubernetes.io/ kubernetes-xenial main
+      EOF 
+      apt-get update
+      apt-get install -y kubelet kubeadm kubectl
+      apt-mark hold kubelet kubeadm kubectl
+
 
 # 初始化master节点
-```
+
 kubeadm init --pod-network-cidr=10.244.0.0/16
 
 sysctl net.bridge.bridge-nf-call-iptables=1
@@ -47,7 +52,7 @@ chown $(id -u):$(id -g) $HOME/.kube/config
 export KUBECONFIG=/etc/kubernetes/admin.conf
  
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
-```
+
 
 # 添加node节点：
 
